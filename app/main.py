f1c4995314cb9d0662ef72417a594c0f38114122
iamdman <34361027+iamdman@users.noreply.github.com>
2021-09-26 15:31:25 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,8 +41,9 @@ trigger_interval = 60
 if "triggerInterval" in settings:
     trigger_interval = settings["triggerInterval"]
     
-# if set to .5 the center of a detected deepstack object will be used to determine if the object is withing a polygon
+# if set to .5 the center of a detected deepstack object will be used to determine if the object is within a polygon
 # if set to a decimal number between such as .05 then 5% from the bottom of the detected object will be used instead
+# a green circle will be applied to the captured image to show what point was used for the IsInsidePolygon function
 polygon_deepstack_bottom_offset = 0.5
 if "polygon_deepstack_bottom_offset" in settings:
     polygon_deepstack_bottom_offset = settings["polygon_deepstack_bottom_offset"]    
